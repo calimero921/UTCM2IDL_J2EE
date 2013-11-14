18bb74b3454740de0e4f2fa564cf88938f15b6cf
bede6362 <bede6362@EB-OR1009018.ad.francetelecom.fr>
2013-11-14 15:10:17 +0100

    mise a jour

--- J2EE-02_FirstServlet/J2EE-02_FirstServlet/src/controlers/HelloWorld.java
+++ J2EE-02_FirstServlet/J2EE-02_FirstServlet/src/controlers/HelloWorld.java
@@ -2,6 +2,7 @@ package controlers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,27 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-//@WebServlet("/HelloWorld")
+@WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 
 	@Override
- 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
- 						 throws ServletException, IOException {
-            PrintWriter out = resp.getWriter();
-            out.println("Hello World");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String firstName = "";
+		String lastName = "";
+		
+		Enumeration<String> fields = req.getAttributeNames();
+		
+		while(fields.hasMoreElements()){
+			if (fields.toString().equals("prenom")) firstName = req.getParameter(fields.toString());
+			if (fields.toString().equals("nom")) lastName = req.getParameter(fields.toString());
+		}		
+		
+        PrintWriter out = resp.getWriter();
+        out.println("Hello World, " + firstName +" "+ lastName);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("Hello World");
-	}
-
-	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1)
-			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.service(arg0, arg1);
+		super.doPost(req, resp);
 	}
 }

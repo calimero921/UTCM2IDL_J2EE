6d8a4e8534735e2f56effa67b4f4ddc5c88cb8fd
bede6362 <bede6362@EB-OR1009018.ad.francetelecom.fr>
2013-11-14 16:52:19 +0100

    Mise a jour

--- J2EE-02_FirstServlet/J2EE-02_FirstServlet/src/controlers/HelloWorld.java
+++ J2EE-02_FirstServlet/J2EE-02_FirstServlet/src/controlers/HelloWorld.java
@@ -16,28 +16,52 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 
+	String firstName = "";
+	String lastName = "";
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-		String firstName = "";
-		String lastName = "";
-		
-		Enumeration<String> fields = req.getAttributeNames();
-		
-		while(fields.hasMoreElements()){
-			if (fields.toString().equals("prenom")) firstName = req.getParameter(fields.toString());
-			if (fields.toString().equals("nom")) lastName = req.getParameter(fields.toString());
-		}		
-		
         PrintWriter out = resp.getWriter();
-        out.println("Hello World, " + firstName +" "+ lastName);
+		out.println("traitement GET");
+        out.println("Hello World, " + this.firstName +" "+ this.lastName);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+
+        PrintWriter out = resp.getWriter();
+		out.println("traitement POST");
+        out.println("Hello World, " + this.firstName +" "+ this.lastName);
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String firstName = "";
+		String lastName = "";
+		
+        PrintWriter out = resp.getWriter();
+		out.println("traitement Service");
+		out.println("Serveur = " + req.getServerName());
+		out.println("Port = " + req.getServerPort());
+		out.println("Session = " + req.getSession());
+		out.println("Methode = " + req.getMethod());
+
+		Enumeration<String> fields = req.getParameterNames();
+		
+		while(fields.hasMoreElements()){
+			String chaine = fields.nextElement();
+			out.println("Parametre : name =>" + chaine +"<, value =>"+ req.getParameter(chaine) + "<");
+			
+			if (chaine.equals("prenom")) this.firstName = req.getParameter(chaine);
+			if (chaine.equals("nom")) this.lastName = req.getParameter(chaine);
+		}		
+        
+        if (req.getMethod().equals("GET")) doGet(req, resp);
+        if (req.getMethod().equals("POST")) doPost(req, resp);
 	}
 }

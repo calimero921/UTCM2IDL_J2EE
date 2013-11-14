88c529c68cd1348c7a24365062ec79074c738363
bede6362 <bede6362@EB-OR1009018.ad.francetelecom.fr>
2013-11-14 11:46:04 +0100

    Mise à jour

--- J2EE-02 FirstServlet/src/controlers/HelloWorld.java
+++ J2EE-02 FirstServlet/src/controlers/HelloWorld.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet(urlPatterns = { "/HelloWorld" })
+@WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
